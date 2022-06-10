484c68b16ac58811c11f8ac45d8d044f1dbc08b9
player1 <david.adam.perry@gmail.com>
2022-06-10 09:19:07 -0500

    trying to fix the postgres thing again

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['DEBUG'] = True
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql", 1)
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
